571f8a0d87e19314f06cb7cb2d518924b151a30e
Maxim Tkachenko <maxim.tkachenko@tomtom.com>
2024-05-20 21:24:26 +0200

    Dark theme is used.

--- src/game/src/commonMain/kotlin/com/gaided/game/ui/MainView.kt
+++ src/game/src/commonMain/kotlin/com/gaided/game/ui/MainView.kt
@@ -1,5 +1,6 @@
 package com.gaided.game.ui
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.ViewModelStore
@@ -7,24 +8,27 @@ import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.gaided.game.GameViewModel
+import com.gaided.game.ui.theme.GameTheme
 
 @Composable
-fun MainView() {
-    val viewModel: GameViewModel = viewModel(
-        GameViewModel::class,
-        viewModelStoreOwner = LocalViewModelStoreOwner.current ?: appViewModelStoreOwner,
-        factory = GameViewModel.Factory()
-    )
+fun MainView() = GameTheme {
+    Surface {
+        val viewModel: GameViewModel = viewModel(
+            GameViewModel::class,
+            viewModelStoreOwner = LocalViewModelStoreOwner.current ?: appViewModelStoreOwner,
+            factory = GameViewModel.Factory()
+        )
 
-    LaunchedEffect(viewModel) {
-        viewModel.start()
+        LaunchedEffect(viewModel) {
+            viewModel.start()
+        }
+
+        chessBoardView(
+            boardUiState = viewModel.board,
+            onSquareTap = viewModel::onSquareClick,
+            onSquareLongPress = viewModel::onSquareLongClick
+        )
     }
-
-    chessBoardView(
-        boardUiState = viewModel.board,
-        onSquareTap = viewModel::onSquareClick,
-        onSquareLongPress = viewModel::onSquareLongClick
-    )
 }
 
 // In desktop app the LocalViewModelStoreOwner.current is null, so this one is used.
