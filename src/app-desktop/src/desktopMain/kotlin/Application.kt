4a7ab9206d664ba1987424eb85afd4b22aceefb8
Maxim Tkachenko <maxim.tkachenko@tomtom.com>
2025-02-12 23:05:01 +0100

    Desktop app compiles.

--- src/app-desktop/src/desktopMain/kotlin/Application.kt
+++ src/app-desktop/src/desktopMain/kotlin/Application.kt
@@ -1,28 +1,21 @@
-import androidx.lifecycle.viewmodel.CreationExtras
-import com.gaided.game.GameViewModel
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
 fun main() {
     println("Gaided desktop.")
 
-    val viewModel = GameViewModel.Factory(config = GameViewModel.Factory.Config(
-        remoteBoardUrl = "http://127.0.0.1:8080",
-        stockfishEngineUrl = "http://127.0.0.1:8081",
-        openAiApiKey = "..."
-    )).create(GameViewModel::class, CreationExtras.Empty)
-
-    val mainLoop = MainScope().launch {
-        viewModel.board.collectLatest {
-            println("board = $it")
-        }
-    }
-
-    runBlocking {
-        mainLoop.join()
-    }
+//    val viewModel = GameViewModel.Factory(config = GameViewModel.Factory.Config(
+//        remoteBoardUrl = "http://127.0.0.1:8080",
+//        stockfishEngineUrl = "http://127.0.0.1:8081",
+//        openAiApiKey = "..."
+//    )).create(GameViewModel::class, CreationExtras.Empty)
+//
+//    val mainLoop = MainScope().launch {
+//        viewModel.board.collectLatest {
+//            println("board = $it")
+//        }
+//    }
+//
+//    runBlocking {
+//        mainLoop.join()
+//    }
 
 //    singleWindowApplication(
 //        state = WindowState(
