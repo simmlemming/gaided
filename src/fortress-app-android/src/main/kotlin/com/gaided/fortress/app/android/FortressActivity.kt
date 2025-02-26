6b758d6004bbcf9ff6273a09745166e6d69619c0
Maxim Tkachenko <maxim.tkachenko@tomtom.com>
2025-02-26 12:40:17 +0100

    TODOs.

--- src/fortress-app-android/src/main/kotlin/com/gaided/fortress/app/android/FortressActivity.kt
+++ src/fortress-app-android/src/main/kotlin/com/gaided/fortress/app/android/FortressActivity.kt
@@ -17,6 +17,7 @@ import com.gaided.logger.Logger
  * - Detect when game ends.
  * - Support config changes.
  * - Support starting from a custom position, for example "5R2/8/K7/8/8/8/7k/6q1 w - - 0 77"
+ * - Add different Stockfish levels.
  */
 
 class FortressActivity : ComponentActivity() {
